78a62e1ab82f897f9565a05b44efbf9d3b1d25d6
Kushagra Krishna <kushagrakrishna@Kushagras-Laptop.local>
2025-05-10 18:27:56 +0530

    first commit

---
+++ instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://dac99ce0906cf04884eb593e997777c0@o4509283385344000.ingest.us.sentry.io/4509283387375616",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
---
+++ instrumentation.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
+}
+
+// Replace with a valid Sentry method or remove the invalid reference
+export const onRequestError = Sentry.captureException;
